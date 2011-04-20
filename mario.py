86a206d7aa4e5b38f8bead01e8100a532c94420d
twobit <twobit@gmx.net>
2011-04-20 15:31:32 +0200

    replaced console output with opengl; gravity fix

--- mario.py
+++ mario.py
@@ -1,29 +1,50 @@
 #!/usr/bin/python
-import os, time, wall
+
 import pygame
 from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import time
 
-WALL = True
+import wall
+
+
+WALL = False # True
 
 class Engine:
 	def __init__(self):
 
-		self.buffer = [[0] * 16 for i in range(15)]
-
-		self.colors = {
-			" ":	("9999cc", "\x1b[40m  \x1b[49m"),		# background
-			"#":	("770000", "\x1b[41m[]\x1b[49m"),		# wall
-			"m":	("aa0000", "\x1b[44mma\x1b[49m"),		# mario
-			"w":	("dddddd", "\x1b[40mww\x1b[49m"),		# clouds
-			"g":	("22aa22", "\x1b[42mgg\x1b[49m"),		# bushes
-		}
-
-		os.system("stty cbreak -echo min 0")
-		os.write(0, "\x1b[2J\x1b[?25l")
-
 		if WALL:
 			wall.init()
 			wall.send("0404")
+
+		self.keys = {}
+		self.buffer = [[0] * 16 for i in range(15)]
+		self.colors = {
+			" ":	"9999cc",		# background
+			"#":	"770000",		# wall
+			"m":	"aa0000",		# mario
+			"w":	"dddddd",		# clouds
+			"g":	"22aa22",		# bushes
+		}
+
+		self.running = True
+
+	def handle_events(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: self.running = False
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE: self.running = False
+				self.keys[event.key] = 1
+			elif event.type == pygame.KEYUP:
+				self.keys[event.key] = 0
+
+	def keystate(self, key, kill=False):
+		if type(key) == str: key = ord(key)
+		c = self.keys.get(key, 0)
+		if kill: self.keys[key] = 0
+		return c
+
 
 	def render(self, buf):
 
@@ -32,29 +53,28 @@ class Engine:
 
 		self.buffer = [row[:] for row in buf]
 
-		# console output
-		os.write(0, "\x1b[1;1H") # clear
-		for row in buf:
-			print "".join(self.colors[c][1] for c in row)
+		# local output
+		glClear(GL_COLOR_BUFFER_BIT)
+
+		glBegin(GL_QUADS)
+		for y, row in enumerate(buf):
+			for x, c in enumerate(row):
+				color = self.colors[c]
+				glColor(*[int(color[i:i+2], 16) / 255.0 for i in range(0,6,2)])
+				glVertex(x, y)
+				glVertex(x+1, y)
+				glVertex(x+1, y+1)
+				glVertex(x, y+1)
+
+		glEnd()
+		pygame.display.flip()
+
 
 		# wall output
-		buf = "".join(self.colors[c][0] for row in buf for c in row)
+		buf = "".join(self.colors[c] for row in buf for c in row)
 		if WALL:
 			wall.frame(buf)
 
-	def exit(self):
-		os.system("stty sane")
-		os.write(0, "\x1b[?25h")
-
-
-
-# input keys
-keys = {}
-def keystate(key, kill=False):
-	if type(key) == str: key = ord(key)
-	c = keys.get(key, 0)
-	if kill: keys[key] = 0
-	return c
 
 
 class Mario:
@@ -70,7 +90,7 @@ class Mario:
 	def update(self):
 
 		# x - movement
-		dx = (keystate(K_RIGHT) - keystate(K_LEFT))
+		dx = (engine.keystate(K_RIGHT) - engine.keystate(K_LEFT))
 		self.move_x += dx
 		if not dx: self.move_x = 0
 
@@ -95,27 +115,33 @@ class Mario:
 					self.x = len(main.level[0]) - 1
 					self.move_x = 0
 
-		# jump
-		if self.move_y == 0 and self.y <= 14 and main.level[self.y + 1][self.x] == "#" and keystate(K_SPACE, True):
-			self.move_y = -26	# jump height
+		# if staning on ground?
+		if self.move_y == 0 and self.y <= 14 and main.level[self.y + 1][self.x] == "#":
+		
+			self.move_y = 0
 			self.move_y_acc = 0
+		
+			# jump
+			if engine.keystate(K_SPACE, True):
+				self.move_y = -26	# jump height
 
-		# gravity
-		self.move_y += 1
+		else:
 
-		# y - movement
-		self.move_y_acc += self.move_y
-		if abs(self.move_y_acc) > 77:	# y - speed
-			s = cmp(self.move_y_acc, 0)
-			self.move_y_acc -= s * 77
+			# gravity
+			self.move_y += 1
 
-			# collision check
-			if self.y + s > 14: return self.die()
-			if main.level[self.y + s][self.x] != "#":
-				self.y += s
-			else:
-				self.move_y = 0
-				self.move_y_accu = 0
+			# y - movement
+			self.move_y_acc += self.move_y
+			if abs(self.move_y_acc) > 77:	# y - speed
+				s = cmp(self.move_y_acc, 0)
+				self.move_y_acc -= s * 77
+
+				# collision check
+				if main.level[self.y + s][self.x] == "#":
+					self.move_y = 0
+					self.move_y_accu = 0
+				else:
+					self.y += s
 	
 	def die(self):
 		main.running = False
@@ -124,8 +150,8 @@ class Mario:
 class Main:
 	def __init__(self):
 
-		self.running = True
-		self.engine = Engine()
+		global engine
+		engine = Engine()
 
 		self.buffer = [[0] * 16 for i in range(15)]
 
@@ -135,29 +161,27 @@ class Main:
 
 		self.mario = Mario()
 
-
 		pygame.display.init()
-		pygame.display.set_mode((1,1))
+		pygame.display.set_mode((320, 300), DOUBLEBUF | OPENGL)
+		glMatrixMode(GL_PROJECTION)
+		glLoadIdentity()
+		gluOrtho2D(0, 16, 15, 0)
+		glMatrixMode(GL_MODELVIEW)
+		glLoadIdentity()
 
 
 	def start(self):
 
-		while self.running:
+		while engine.running:
 
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT: return
-				elif event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_ESCAPE: return
-					keys[event.key] = 1
-				elif event.type == pygame.KEYUP:
-					keys[event.key] = 0
+			engine.handle_events()
+
 
 			self.mario.update()
 
 			self.render()
 			time.sleep(0.01)
 
-#		raw_input()
 
 	def render(self):
 
@@ -169,11 +193,8 @@ class Main:
 		# mario
 		self.buffer[self.mario.y][self.mario.x - self.cam_pos] = "m"
 
-		self.engine.render(self.buffer)
+		engine.render(self.buffer)
 
-
-	def exit(self):
-		self.engine.exit()
 
 
 if __name__ == "__main__":
@@ -187,6 +208,6 @@ if __name__ == "__main__":
 	except KeyboardInterrupt:
 		pass
 
-	main.exit()
+#	main.exit()
 
 
